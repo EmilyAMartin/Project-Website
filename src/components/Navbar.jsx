20b25cba4ed0dad8e5af3884b31202838e47f003
Emily Martin <163541100+EmilyAMartin@users.noreply.github.com>
2025-06-10 12:44:44 -0230

    Setting up react app

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+const Navbar = () => {
+	return (
+		<Box sx={{ flexGrow: 1 }}>
+			<AppBar position='static'>
+				<Toolbar>
+					<Typography
+						variant='h6'
+						component='div'
+						sx={{ flexGrow: 1 }}
+					>
+						Website Name
+					</Typography>
+					<Button color='inherit'>Home</Button>
+					<Button color='inherit'>About</Button>
+					<Button color='inherit'>Contact</Button>
+				</Toolbar>
+			</AppBar>
+		</Box>
+	);
+};
+
+export default Navbar;
